177efbdd3d992876ce27368d6dd2a2ff3e659665
akramazegarh <74309268+akramazegarh@users.noreply.github.com>
2025-01-12 00:12:36 +0100

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -7,6 +7,9 @@
 std::vector<int> initVector(int method)
 {
     std::vector<int> aVector;
+    const int aSize = 5;
+    int aArray[] = {11, 54, 54, 00 };
+
     switch (method)
     {
         case 1 :
@@ -14,6 +17,17 @@ std::vector<int> initVector(int method)
             break;
         case 2 :
             aVector = std::vector(10,5);
+            break;
+        case 3 :
+            for (int i = 0 ; i < aSize; i++)
+            {
+                aVector.push_back(i);
+            }
+
+            std::for_each(aArray , aArray + 4,[&](const int value)
+            {
+                aVector.insert(aVector.begin(), value); 
+            });            
             break;
         default :
             aVector = std::vector({888888});
@@ -28,11 +42,11 @@ int main()
 {
     std::cout << std::endl << "[MAIN_START]" << std::endl << std::endl;
 
-    int arr[] = {1, 2, 3};
+    int arr[] = {1, 2, 3, 5, 6};
 
-    std::for_each(arr,arr + 3, [](int aIniType)
+    std::for_each(arr,arr + 8, [](int aIniType)
     {
-        std::cout << std::endl << "INUT TYPE : " << aIniType << std::endl << std::endl;
+        std::cout << std::endl << "INIT TYPE : " << aIniType << std::endl << std::endl;
         auto aVector = initVector(aIniType);
 
         for (int i : aVector)
