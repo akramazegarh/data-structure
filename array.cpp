140a278e322c911681987a6df11e3e6bba1a6542
akramazegarh <74309268+akramazegarh@users.noreply.github.com>
2025-01-11 19:14:34 +0100

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
-// FOR std::size 
+// std::size 
 #include <iterator>
+// std::span
+#include <span>
+// std::for_each
+#include <algorithm>
+
 
 void fillArray(int iArray[], const int iArraySize)
 {
-    std::cout << "[FILL] <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< " << std::endl;
-
+    std::cout << "[FILL_START]" << std::endl << std::endl;
     for (int i = 0 ; i<iArraySize ; i++)
     {
-        std::cout << "[FILL] REF OF  "<< i << " >> "<< &iArray[i] << std::endl;
         iArray[i] = i + 777;
-        std::cout << "[FILL] VALUE "<< iArray[i] << std::endl;
     }
-    std::cout << "[FILL] =============================== " << std::endl;
+
+    std::cout << "[IERATOR]" << std::endl;
     int* aBegin = iArray;
     int* aEnd = iArray + iArraySize;
-
     for (int* iter = aBegin ; iter != aEnd; iter++)
     {
-        std::cout << "[FILL][OUT] REF >> " << iter << std::endl;
+        std::cout << "[FILL][OUT] REF    >> " << iter << std::endl;
         std::cout << "[FILL][OUT] VALUES >> " << *iter << std::endl;
     }
 
-    std::cout << "[FILL] >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> " << std::endl;
+    std::cout << "[SPAN]" << std::endl;
+    for (const int& value : std::span(iArray, iArraySize))
+    {
+        std::cout << "[FILL][OUT] REF    >> " << &value << std::endl;
+        std::cout << "[FILL][OUT] VALUES >> " << value << std::endl;
+    }
+
+    std::cout << "[for_each]" << std::endl;
+    std::for_each(aBegin, aEnd, [](int value)
+    {
+        std::cout << "[FILL][OUT] REF    >> " << &value << std::endl;
+        std::cout << "[FILL][OUT] VALUES >> " << value << std::endl;
+    });
+
+    std::cout << std::endl << "[FILL_END]" << std::endl << std::endl;
 }
 
 
 
 template <typename T, size_t N>
 void computeSize(T (&iArray)[N]) {
-    std::cout << "[SIZE] ARRAY SIZE  : " << N << std::endl;
+    std::cout << "[SIZE] ARRAY SIZE  >> " << N << std::endl;
 }
 
 int main()
 {
-    std::cout << "[MAIN] Start\n" << std::endl;
+    std::cout << std::endl << "[MAIN_START]" << std::endl << std::endl;
 
     const int aArraySize = 4;
     int aArray[aArraySize];
@@ -42,7 +58,7 @@ int main()
     fillArray(aArray, aArraySize);
     computeSize(aArray);
 
-    std::cout << "\n[MAIN] End" << std::endl;
+    std::cout << std::endl << "[MAIN_END]" << std::endl << std::endl;
 
     return 0;
 }
